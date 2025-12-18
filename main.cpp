31b755d3b1238e86365cb1b6d5457d902c2c6617
addy <addyjaime@gmail.com>
2025-12-18 18:49:07 -0500

    checking if the rooms are available

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 struct Room
 {
   int room_number;
-  string available;
+  bool available;
   float price;
   string type;
 };
@@ -17,20 +17,23 @@ struct Room
 
 int main(){
   cout << "Welcome to Hotel Management System \n";
-  // cin.get();
+  cin.get();
 
 // vector is also a data strucwture from the Standard Library that stores a collection of elements similar to an array but more flexible // sintaxis es vector DATA TYPE y luego un nombre
   vector<Room> rooms;
 
 // aqui utilizamos el molde para crear habitaciones
-  Room room_1 = {122, "Occupied", 20.20f, "basic" };
+  Room room_1 = {122, true, 20.20, "basic" };
 
-  Room room_2 = {123, "Available", 40.20f, "medium"};
+  Room room_2 = {123, false, 40.20, "medium"};
+
+  Room room_3 = {124, true, 50.20, "Large"};
 
   rooms.push_back(room_1);
   rooms.push_back(room_2);
+  rooms.push_back(room_3);
 
-  // 
+  //  formatea los precios con 2 decimales
   cout << fixed << setprecision(2);
 
   for (int i = 0; i < rooms.size(); i++)
@@ -41,6 +44,34 @@ int main(){
     cout << "Type: " << rooms[i].type << "\n";
     cout << "-----------------------------\n";
   }
+
+for (int i = 0; i < rooms.size(); i++)
+{
+  if (rooms[i].available)
+  {
+    cout << "Habitaciones disponibles: " << rooms[i].available << "\n";
+    // return 0;
+  }
+}
+
+int room_number;
+
+cout << "What room number would you like? ";
+cin >> room_number;
+
+cout << room_number << "\n";
+
+for (int i = 0; i < rooms.size(); i++)
+{
+  if (room_number == rooms[i].room_number)
+  {
+    cout << "Room found"<< "\n";
+   return 0;
+  } else {
+    cout << "Room not found" << "\n";
+  }
   
-  return 0;
+}
+
+
 }
