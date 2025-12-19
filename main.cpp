39a554481d93c030074f10ecbe908239047478ec
addy <addyjaime@gmail.com>
2025-12-19 13:16:15 -0500

    adding converstion to decimal numbers

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ struct Room
 {
   int room_number;
   bool available;
-  float price;
+  int price_cents;
   string type;
 };
 
@@ -23,36 +23,41 @@ int main(){
   vector<Room> rooms;
 
 // aqui utilizamos el molde para crear habitaciones
-  Room room_1 = {122, true, 20.20, "basic" };
+  Room room_1 = {122, true, 2020, "basic" };
 
-  Room room_2 = {123, false, 40.20, "medium"};
+  Room room_2 = {123, false, 4020, "medium"};
 
-  Room room_3 = {124, true, 50.20, "Large"};
+  Room room_3 = {124, true, 5020, "Large"};
 
   rooms.push_back(room_1);
   rooms.push_back(room_2);
   rooms.push_back(room_3);
 
   //  formatea los precios con 2 decimales
+// setprecision() NO cambia el número
+// solo cambia cómo se muestra
   cout << fixed << setprecision(2);
 
   for (int i = 0; i < rooms.size(); i++)
   {
     cout << "Room number: " << rooms[i]. room_number << "\n";
     cout << "Status: " << rooms[i].available << "\n";
-    cout << "Price: $" << rooms[i].price << "\n";
+    cout << "Price: $" << rooms[i].price_cents / 100.0<< "\n";
     cout << "Type: " << rooms[i].type << "\n";
     cout << "-----------------------------\n";
   }
+
+int availablecount = 0;
 
 for (int i = 0; i < rooms.size(); i++)
 {
   if (rooms[i].available)
   {
-    cout << "Habitaciones disponibles: " << rooms[i].available << "\n";
-    // return 0;
+    // here we count the rooms available for every pass acumulamos 
+    availablecount++;
   }
 }
+cout << "Habitaciones disponibles: " << availablecount << "\n";
 
 int room_number;
 
@@ -65,12 +70,12 @@ for (int i = 0; i < rooms.size(); i++)
 {
   if (room_number == rooms[i].room_number)
   {
-    cout << "Room found"<< "\n";
-   return 0;
+    cout << "Room available"<< "\n";
+
   } else {
-    cout << "Room not found" << "\n";
+    cout << "Room not available" << "\n";
   }
-  
+  return 0;  
 }
 
 
